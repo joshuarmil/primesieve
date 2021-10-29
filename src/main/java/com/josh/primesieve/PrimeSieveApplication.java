48d18099f897f1edbc4f9f5d6bdc05daefe7cb26
Josh Miller <31965595+joshuarmil@users.noreply.github.com>
2021-10-29 18:26:51 -0400

    Initial Spring Boot commit

---
+++ src/main/java/com/josh/primesieve/PrimeSieveApplication.java
@@ -0,0 +1,13 @@
+package com.josh.primesieve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeSieveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeSieveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josh/primesieve/PrimeSieveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josh.primesieve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeSieveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
