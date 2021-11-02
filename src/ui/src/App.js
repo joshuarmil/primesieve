01f658427d28009b57cc927146417a91523262b5
Josh Miller <31965595+joshuarmil@users.noreply.github.com>
2021-11-01 22:46:26 -0400

    Connect UI to Spring Boot endpoint

--- src/ui/src/App.js
+++ src/ui/src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
+import logo from './sieve.png';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Input, InputGroup } from 'reactstrap';
 
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      value: '',
+      valueValid: undefined,
+      submittedValue: 0,
+      responseValid: false,
+      primes: []
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = async() => {
+    if (this.state.valueValid) {
+      this.setState({responseValid: false})
+      const response = await fetch(`/primes?value=${this.state.value}`);
+      const body = await response.json();
+      if (this.validateResponse(body)) {
+        this.setState({submittedValue: this.state.value, primes: body, responseValid: true});
+      }
+    } 
+  }
+
+  handleChange({ target }) {
+    this.setState({
+      value: target.value
+    });
+
+    if (target.value == "") {
+      this.setState({valueValid: undefined})
+    } else if (this.validateInput(target.value)) {
+      this.setState({valueValid: true})
+    } else {
+      this.setState({valueValid: false})
+    }
+  }
+
+  validateInput(value) {
+    // RegEx testing that all characters are digits 0-9
+    return /^\d+$/.test(value);
+  }
+
+
+  validateResponse(response) {
+    return Array.isArray(response);
+  }
+
+  render() {
+    const {submittedValue, primes} = this.state;
+
+    function PrimeResults() {
+      return (
+        <div className="Prime-table">
+          <h2>Total primes up to {submittedValue} : {primes.length}</h2>
+          <div class="row row-cols-auto">
+            {primes.map((prime, i) =>
+                <div class="col col-lg-1" key={i}> {prime} </div>
+            )}
+          </div>
+        </div>
+      );
+    }
+
+    let results;
+
+    if (this.state.responseValid) {
+      results = <PrimeResults/>
+    } else {
+      results = <div></div>
+    }
+
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Generate Lesser or Equal Primes</h2>
+          </header>
+          <div className="App-body">
+            <InputGroup className="Prime-form" >
+              <Input 
+                type="text" 
+                name="inputBox" 
+                className="form-control"
+                placeholder="Enter integer here" 
+                value={ this.state.value }
+                onChange={ this.handleChange }
+                id="validationDefault05"
+                required
+              />
+              <Button value="Send" onClick={ this.handleClick }>Calculate</Button>
+            </InputGroup>
+            {this.state.valueValid == false ? <span style={{color: "red"}}>Please enter a valid integer</span> : ''}
+            {results}
+          </div>
+        </div>
+    );
+  }
+}
 export default App;
--- src/ui/src/App.test.js
+++ src/ui/src/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+ 
 import App from './App';
-
-test('renders learn react link', () => {
+ 
+test('render error on invalid input', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const inputEl = screen.getByRole('textbox', { placeholder: "inputBox" });
+  expect(inputEl).toBeInTheDocument();
+  expect(inputEl).toHaveAttribute("type", "text");
+
+  //userEvent.type(inputEl, "test");
+  fireEvent.change(inputEl, { target: {value: "test"}});
+
+  expect(inputEl).toHaveValue("test");
+  const resultElement = screen.getByText(/Please enter a valid integer/i);
+  expect(resultElement).toBeInTheDocument();
 });
